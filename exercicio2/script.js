0a473beb17bd689b78a1dc0ab874fcb33285ec1c
Arthur Jordi Alves Silva <114433340+ArthurJ99@users.noreply.github.com>
2022-11-01 18:40:33 -0300

    Exercicios de strings rrealizados

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,15 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+
+// Exercicio 02
+// a) e b)
+// com os espaços
+console.log(minhaString.length)
+//sem os espaços
+console.log(minhaString.trim().length)
+
+// c)
+
+const novaString = minhaString.replace("________","sticioso")
+console.log(minhaString)
+console.log(novaString)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,16 @@
 //Crie a const para a frase aqui
+//Exercicio 03
+
+// a)
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\" "
+console.log(frase)
+// b)
+const novaFrase = frase.replace("verde","rosa").replace("azul","laranja")
+console.log(novaFrase)
+// c)
+console.log(novaFrase.includes("verde"))
+console.log(novaFrase.includes("laranja"))
+
+// Extra
+
+console.log(novaFrase.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR"))
